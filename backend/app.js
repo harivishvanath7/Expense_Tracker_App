ed8f759c3842e4380a282d600b1cd590c099debc
HARI VISHVANATH S <harivishvanath7@gmail.com>
2024-03-18 17:51:53 +0530

    latest commit

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { db } = require("./db/db");
+const { readdirSync } = require("fs");
 
 const app = express();
 
@@ -11,11 +13,13 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// Routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
 const server = () => {
+  db;
   app.listen(PORT, () => {
     console.log("Listening to PORT:", PORT);
   });
---
+++ backend/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const db = async () => {
+  try {
+    mongoose.set("strictQuery", false); // Disables strict mode for queries
+    await mongoose.connect(process.env.MONGODB_URL); // Connects to the MongoDB database
+    console.log("DB is Connected"); // Logs a success message if the connection is successful
+  } catch (error) {
+    console.error("DB Connection Error:"); // Logs an error message if there's an error connecting to the database
+  }
+};
+
+module.exports = db;
